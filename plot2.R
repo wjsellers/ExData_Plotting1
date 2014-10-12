98218746557fef81710d7820b70b76f8e128b823
William J Sellers <wjsellers@gmail.com>
2014-10-12 14:02:29 -0700

    added comment about original data source

--- plot2.R
+++ plot2.R
@@ -8,6 +8,7 @@ setwd("~/Repos/ExData_Plotting1")
 
 
 # read in data for 2007-02-01 and 2007-02-02 using sqldf package
+# original data from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 library(sqldf)
 day1 <- read.csv.sql("~/Documents/Data/Coursera/Exploratory Data Analysis/CourseProject1/household_power_consumption.txt", 
                      sql = 'select * from file where Date == "1/2/2007"', 
